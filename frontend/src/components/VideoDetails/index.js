c89ce305b87e5915bb686b1e5aa4deba4f715bfc
sooraj888 <soorajsagar888@gmail.com>
2024-04-07 11:11:39 +0530

    added like share designs

--- frontend/src/components/VideoDetails/index.js
+++ frontend/src/components/VideoDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ReactPlayer from "react-player";
 import "./videoDetails.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
@@ -6,12 +6,20 @@ import { Avatar } from "@chakra-ui/react";
 import { ago, convertSecondsToTime } from "../../utils/util";
 import { contents } from "../VideoList/data";
 import { MenuContext } from "../../context/MainContext";
+import { BiLike } from "react-icons/bi";
+import { BiDislike } from "react-icons/bi";
+import { BiSolidDislike } from "react-icons/bi";
+import { BiSolidLike } from "react-icons/bi";
+import { PiShareFatLight } from "react-icons/pi";
+import { useAlert } from "react-alert";
 
 export default function VideoDetails() {
   const [searchParams] = useSearchParams();
   const videoId = searchParams.get("v");
   const navigation = useNavigate();
   const { isDarkTheme } = useContext(MenuContext);
+  const [isLiked, setIsLiked] = useState(null);
+  const bottomAlert = useAlert();
 
   if (!videoId) {
     navigation(`/`);
@@ -25,6 +33,24 @@ export default function VideoDetails() {
     window.scrollTo(0, 0);
     navigation(`/video?v=${id}`, {
       preventScrollReset: false,
+    });
+  };
+
+  const onclickLikeDisLike = (isLike) => {
+    setIsLiked((prev) => {
+      if (prev == isLike) {
+        return null;
+      } else {
+        return isLike;
+      }
+    });
+  };
+
+  const handleOnShare = async () => {
+    const currentUrl = window.location.href;
+    await navigator.clipboard.writeText(currentUrl);
+    bottomAlert.success("Share Link Copied successfully", {
+      position: "top center",
     });
   };
 
@@ -44,31 +70,82 @@ export default function VideoDetails() {
           </div>
           <h1>{data.title}</h1>
           <div className="videoDetailsOwner">
-            <Avatar
-              onClick={(e) => {
-                e.stopPropagation();
-                onClickUser(data.user.userId);
-              }}
-              className="videoDetailsAv"
-              size={"sm"}
-              name={String(data.user?.name || "")}
-              src={String(data.user?.avatar?.url)}
-            />
-
-            <h2
-              onClick={(e) => {
-                e.stopPropagation();
-                onClickUser(data.user.userId);
-              }}
-              style={{
-                fontWeight: "600",
-                textTransform: "capitalize",
-              }}
-            >
-              {data.user.userName}
-            </h2>
+            <span>
+              <Avatar
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onClickUser(data.user.userId);
+                }}
+                className="videoDetailsAv"
+                size={"sm"}
+                name={String(data.user?.name || "")}
+                src={String(data.user?.avatar?.url)}
+              />
+              <h2
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onClickUser(data.user.userId);
+                }}
+                style={{
+                  fontWeight: "600",
+                  textTransform: "capitalize",
+                }}
+              >
+                {data.user.userName}
+              </h2>
+            </span>
+            <span className="userInputSubContainer">
+              <span className="likeContainer">
+                <span
+                  onClick={() => {
+                    onclickLikeDisLike(true);
+                  }}
+                  style={{
+                    background: isDarkTheme
+                      ? "rgb(44, 43, 43)"
+                      : "rgb(230, 230, 230)",
+                  }}
+                >
+                  {isLiked ? <BiSolidLike /> : <BiLike />}
+                  <pre className="prevent-select">{123}</pre>
+                </span>
+                <div>
+                  <div></div>
+                </div>
+                <span
+                  onClick={() => {
+                    onclickLikeDisLike(false);
+                  }}
+                  style={{
+                    background: isDarkTheme
+                      ? "rgb(44, 43, 43)"
+                      : "rgb(230, 230, 230)",
+                  }}
+                >
+                  {!isLiked && isLiked != null ? (
+                    <BiSolidDislike />
+                  ) : (
+                    <BiDislike />
+                  )}
+                  <pre className="prevent-select">{123}</pre>
+                </span>
+              </span>
+              <span
+                className="shareContainer"
+                onClick={handleOnShare}
+                style={{
+                  background: isDarkTheme
+                    ? "rgb(44, 43, 43)"
+                    : "rgb(230, 230, 230)",
+                }}
+              >
+                <PiShareFatLight />
+                <p className="prevent-select">Share</p>
+              </span>
+            </span>
           </div>
         </div>
+
         <div className="videoDetailsList">
           <div className="videoDetailsListSubContainer">
             {contents.map((video, key) => {
